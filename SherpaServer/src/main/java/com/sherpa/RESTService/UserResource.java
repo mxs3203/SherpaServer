8f42ecd0772b2c42aa7b3916c0b7124d8e712022
ZGDVeljo <axv4685@rit.edu>
2016-11-16 22:27:00 +0100

    pom i web xml izmjene

--- SherpaServer/src/main/java/com/sherpa/RESTService/UserResource.java
+++ SherpaServer/src/main/java/com/sherpa/RESTService/UserResource.java
@@ -3,16 +3,41 @@ package com.sherpa.RESTService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.sherpa.dao.UserHome;
+import com.sherpa.dto.User;
 
 
 @Path("/users")
 public class UserResource{
 
     @GET
-    @Produces("text/plain")
-    public String users(){
+    @Produces(MediaType.APPLICATION_JSON)
+    public User users(){
+    	//UserHome uh = new UserHome();
+    	//User user = uh.findById(10);
 
-        return "Branko";
+        return null;
 
     }
+    
+    public static void main (String[] args){
+    	
+    	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    
+    	Session session = sessionFactory.getCurrentSession();
+    	
+    	session.beginTransaction();
+
+
+    	User uh = session.get(User.class, 13);
+    	System.out.println(uh);
+    	
+    	
+    };
 }
